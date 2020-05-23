3515e9db7e6cfcdbf32533e3e880158b0f83a5b0
codie <vikashsingh.codie@gmail.com>
2020-05-23 19:24:47 +0530

    Initial commit

---
+++ app/toasty/src/main/java/com/codie/toasty/FailedToast.java
@@ -0,0 +1,27 @@
+package com.codie.toasty;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FailedToast extends LinearLayout {
+
+    public FailedToast(Context context, String message) {
+        super(context);
+        show(context, message);
+    }
+
+    private void show(Context context, String message) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fail_toast,this,false);
+        TextView textView = findViewById(R.id.text);
+        textView.setText(message);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view);
+        toast.show();
+    }
+}
